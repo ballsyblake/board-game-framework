decee91713b6cd84e229edb95de2b64b4338deb3
bernie-cm <170481602+bernie-cm@users.noreply.github.com>
2026-03-31 21:52:03 +1100

    Add constructor and template method

---
+++ BoardGameFramework/Game.cs
@@ -0,0 +1,34 @@
+namespace BoardGameFramework;
+
+public abstract class Game
+{
+    protected Board board;
+    protected Player currentPlayer;
+    protected List<Player> players = new List<Player>();
+    protected IDisplay display;
+    protected HistoryManager historyManager;
+    protected GameSaver gameSaver;
+
+    protected Game(IDisplay display, HistoryManager historyManager, GameSaver gameSaver)
+    {
+        this.display = display;
+        this.historyManager = historyManager;
+        this.gameSaver = gameSaver;
+    }
+
+    public void PlayGame()
+    {
+        InitialiseGame();
+        int currentIndex = 0;
+        while (!EndOfGame())
+        {
+            MakePlay(currentIndex);
+            currentIndex = (currentIndex + 1) % players.Count;
+        }
+        PrintWinner();
+    }
+    protected abstract void InitialiseGame();
+    protected abstract bool EndOfGame();
+    protected abstract void MakePlay(int player);
+    protected abstract void PrintWinner();
+}
